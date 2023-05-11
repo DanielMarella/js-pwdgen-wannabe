76759f1336c9797125d67f6482b504b328b46db4
DanielMarella <marella.daniel96@gmail.com>
2023-05-11 16:45:41 +0200

    Print the password on the document

--- js/myscript.js
+++ js/myscript.js
@@ -11,3 +11,7 @@ document.getElementById('yourSurname').innerHTML = userSurname;
 const userFavoriteColor = (prompt('Inserisci il tuo colore preferito'));
 
 document.getElementById('yourFavoriteColor').innerHTML = userFavoriteColor;
+
+const userPassword = userName + userSurname + userFavoriteColor + '21'
+
+document.getElementById('yourPassword').innerHTML = userPassword;
