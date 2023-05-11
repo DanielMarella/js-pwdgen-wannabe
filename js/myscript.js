0422fdadceecc461c419eab7357b0db9b2242b60
DanielMarella <marella.daniel96@gmail.com>
2023-05-11 16:42:50 +0200

    adding surname request

--- js/myscript.js
+++ js/myscript.js
@@ -4,6 +4,8 @@ const userName = (prompt('Inserisci il tuo nome'));
 
 document.getElementById('yourName').innerHTML = userName;
 
+const userSurname = (prompt('Inserisci il tuo cognome'));
 
+document.getElementById('yourSurname').innerHTML = userSurname;
 
 
